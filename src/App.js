96c456f3dc8cd7cb2d49389dd783e2a2d885c212
Gabriela Muniz <gabrielanamu@live.com>
2022-11-01 20:42:27 -0300

    adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
 import pokemonList from './data';
 import Pokedex from './components/Pokedex';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="Ap p">
-      <Pokedex pokemonList={ pokemonList } />
+    <div className="App">
+      <Link to="/">Pokedex</Link>
+      <Link to="/about">About Pokedex</Link>
+      <Switch>
+        <Route exact path="/about" render={ () => <About /> } />
+        <Route
+          exact
+          path="/"
+          render={ (props) => <Pokedex { ...props } pokemonList={ pokemonList } /> }
+        />
+      </Switch>
     </div>
   );
 }
