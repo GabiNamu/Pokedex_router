78b50f2c1c483c978a3bdc0eefe280fcaee9bfa6
Gabriela Muniz <gabrielanamu@live.com>
2022-11-01 20:18:19 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Pokedex from './components/Pokedex';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Ap p">
       <Pokedex pokemonList={ pokemonList } />
     </div>
   );
